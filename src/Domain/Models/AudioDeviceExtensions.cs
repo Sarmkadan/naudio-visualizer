011be8e1ac12a07edb1b088e6e919b688b4e42a2
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 09:21:58 +0300

    readability pass over AudioDeviceExtensions.

--- src/Domain/Models/AudioDeviceExtensions.cs
+++ src/Domain/Models/AudioDeviceExtensions.cs
@@ -1,7 +1,7 @@
 namespace NAudioVisualizer.Domain.Models;
 
 /// <summary>
-/// Extensions for <see cref="AudioDevice"/>.
+/// Provides extension methods for <see cref="AudioDevice"/> to enhance device manipulation and querying.
 /// </summary>
 public static class AudioDeviceExtensions
 {
@@ -12,9 +12,11 @@ public static class AudioDeviceExtensions
     /// <param name="sampleRate">The sample rate to check for support.</param>
     /// <returns><see langword="true"/> if the device supports the sample rate; otherwise, <see langword="false"/>.</returns>
     /// <exception cref="ArgumentNullException"><paramref name="device"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentOutOfRangeException"><paramref name="sampleRate"/> is less than or equal to zero.</exception>
     public static bool IsSampleRateSupported(this AudioDevice device, int sampleRate)
     {
         ArgumentNullException.ThrowIfNull(device);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(sampleRate);
         return device.SupportsSampleRate(sampleRate);
     }
 
@@ -40,5 +42,89 @@ public static class AudioDeviceExtensions
     {
         ArgumentNullException.ThrowIfNull(device);
         return device.IsDefault;
+    }
+
+    /// <summary>
+    /// Gets the default sample rate for the <paramref name="device"/>.
+    /// </summary>
+    /// <param name="device">The <see cref="AudioDevice"/> to get the default sample rate for.</param>
+    /// <returns>The default sample rate in Hz.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="device"/> is <see langword="null"/>.</exception>
+    public static int GetDefaultSampleRate(this AudioDevice device)
+    {
+        ArgumentNullException.ThrowIfNull(device);
+        return device.DefaultSampleRate;
+    }
+
+    /// <summary>
+    /// Gets the number of input channels for the <paramref name="device"/>.
+    /// </summary>
+    /// <param name="device">The <see cref="AudioDevice"/> to get the channel count for.</param>
+    /// <returns>The number of input channels.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="device"/> is <see langword="null"/>.</exception>
+    public static int GetChannelCount(this AudioDevice device)
+    {
+        ArgumentNullException.ThrowIfNull(device);
+        return device.ChannelCount;
+    }
+
+    /// <summary>
+    /// Gets the bit depth for the <paramref name="device"/>.
+    /// </summary>
+    /// <param name="device">The <see cref="AudioDevice"/> to get the bit depth for.</param>
+    /// <returns>The bit depth in bits (16, 24, or 32).</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="device"/> is <see langword="null"/>.</exception>
+    public static int GetBitDepth(this AudioDevice device)
+    {
+        ArgumentNullException.ThrowIfNull(device);
+        return device.BitDepth;
+    }
+
+    /// <summary>
+    /// Gets the manufacturer name for the <paramref name="device"/>.
+    /// </summary>
+    /// <param name="device">The <see cref="AudioDevice"/> to get the manufacturer for.</param>
+    /// <returns>The manufacturer name.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="device"/> is <see langword="null"/>.</exception>
+    public static string GetManufacturer(this AudioDevice device)
+    {
+        ArgumentNullException.ThrowIfNull(device);
+        return device.Manufacturer;
+    }
+
+    /// <summary>
+    /// Gets the device name.
+    /// </summary>
+    /// <param name="device">The <see cref="AudioDevice"/> to get the name for.</param>
+    /// <returns>The device name.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="device"/> is <see langword="null"/>.</exception>
+    public static string GetName(this AudioDevice device)
+    {
+        ArgumentNullException.ThrowIfNull(device);
+        return device.Name;
+    }
+
+    /// <summary>
+    /// Gets the device capabilities.
+    /// </summary>
+    /// <param name="device">The <see cref="AudioDevice"/> to get capabilities for.</param>
+    /// <returns>The <see cref="DeviceCapabilities"/> flags.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="device"/> is <see langword="null"/>.</exception>
+    public static DeviceCapabilities GetCapabilities(this AudioDevice device)
+    {
+        ArgumentNullException.ThrowIfNull(device);
+        return device.Capabilities;
+    }
+
+    /// <summary>
+    /// Determines whether the device is currently available.
+    /// </summary>
+    /// <param name="device">The <see cref="AudioDevice"/> to check availability for.</param>
+    /// <returns><see langword="true"/> if the device is available; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="device"/> is <see langword="null"/>.</exception>
+    public static bool IsAvailable(this AudioDevice device)
+    {
+        ArgumentNullException.ThrowIfNull(device);
+        return device.IsAvailable;
     }
 }
