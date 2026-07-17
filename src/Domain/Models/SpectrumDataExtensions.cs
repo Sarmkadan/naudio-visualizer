afffd27316aa9475e83655d715e4874f5312494a
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 11:37:50 +0300

    small fix.

--- src/Domain/Models/SpectrumDataExtensions.cs
+++ src/Domain/Models/SpectrumDataExtensions.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+
 namespace NAudioVisualizer.Domain.Models;
 
 /// <summary>
@@ -5,61 +8,72 @@ namespace NAudioVisualizer.Domain.Models;
 /// </summary>
 public static class SpectrumDataExtensions
 {
-    /// <summary>
-    /// Determines if the spectrum data has a valid peak frequency and magnitude.
-    /// </summary>
-    /// <param name="spectrumData">The spectrum data to validate.</param>
-    /// <returns><c>true</c> if the spectrum data has a valid peak frequency and magnitude; otherwise, <c>false</c>.</returns>
-    /// <exception cref="ArgumentNullException"><paramref name="spectrumData"/> is <c>null</c>.</exception>
-    public static bool HasValidPeak(this SpectrumData spectrumData)
-    {
-        ArgumentNullException.ThrowIfNull(spectrumData);
+	/// <summary>
+	/// Determines if the spectrum data has a valid peak frequency and magnitude.
+	/// </summary>
+	/// <param name="spectrumData">The spectrum data to validate.</param>
+	/// <returns><c>true</c> if the spectrum data has a valid peak frequency and magnitude; otherwise, <c>false</c>.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="spectrumData"/> is <c>null</c>.</exception>
+	public static bool HasValidPeak(this SpectrumData spectrumData)
+	{
+		ArgumentNullException.ThrowIfNull(spectrumData);
 
-        return spectrumData.PeakFrequency > 0 && spectrumData.PeakMagnitude > 0;
-    }
+		return spectrumData.PeakFrequency > 0 && spectrumData.PeakMagnitude > 0;
+	}
 
-    /// <summary>
-    /// Calculates the frequency index of the peak frequency in the spectrum data.
-    /// </summary>
-    /// <param name="spectrumData">The spectrum data to calculate the frequency index for.</param>
-    /// <returns>The frequency index of the peak frequency.</returns>
-    /// <exception cref="ArgumentNullException"><paramref name="spectrumData"/> is <c>null</c>.</exception>
-    public static int GetPeakFrequencyIndex(this SpectrumData spectrumData)
-    {
-        ArgumentNullException.ThrowIfNull(spectrumData);
+	/// <summary>
+	/// Calculates the frequency index of the peak frequency in the spectrum data.
+	/// </summary>
+	/// <param name="spectrumData">The spectrum data to calculate the frequency index for.</param>
+	/// <returns>The frequency index of the peak frequency.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="spectrumData"/> is <c>null</c>.</exception>
+	/// <exception cref="ArgumentException"><paramref name="spectrumData"/> has empty frequency data.</exception>
+	public static int GetPeakFrequencyIndex(this SpectrumData spectrumData)
+	{
+		ArgumentNullException.ThrowIfNull(spectrumData);
 
-        var frequencies = spectrumData.GetFrequencies();
-        var peakFrequency = spectrumData.PeakFrequency;
+		var frequencies = spectrumData.GetFrequencies();
+		ArgumentNullException.ThrowIfNull(frequencies);
 
-        for (int i = 0; i < frequencies.Length; i++)
-        {
-            if (frequencies[i] == peakFrequency)
-            {
-                return i;
-            }
-        }
+		var peakFrequency = spectrumData.PeakFrequency;
+		if (peakFrequency <= 0)
+		{
+			throw new InvalidOperationException("Peak frequency must be positive.");
+		}
 
-        throw new InvalidOperationException("Peak frequency not found in frequencies array.");
-    }
+		for (int i = 0; i < frequencies.Length; i++)
+		{
+			if (frequencies[i] == peakFrequency)
+			{
+				return i;
+			}
+		}
 
-    /// <summary>
-    /// Normalizes the spectrum data to a maximum magnitude of 1.
-    /// </summary>
-    /// <param name="spectrumData">The spectrum data to normalize.</param>
-    /// <exception cref="ArgumentNullException"><paramref name="spectrumData"/> is <c>null</c>.</exception>
-    public static void NormalizeToOne(this SpectrumData spectrumData)
-    {
-        ArgumentNullException.ThrowIfNull(spectrumData);
+		throw new InvalidOperationException("Peak frequency not found in frequencies array.");
+	}
 
-        var maxMagnitude = spectrumData.GetData().Max();
+	/// <summary>
+	/// Normalizes the spectrum data to a maximum magnitude of 1.
+	/// </summary>
+	/// <param name="spectrumData">The spectrum data to normalize.</param>
+	/// <exception cref="ArgumentNullException"><paramref name="spectrumData"/> is <c>null</c>.</exception>
+	public static void NormalizeToOne(this SpectrumData spectrumData)
+	{
+		ArgumentNullException.ThrowIfNull(spectrumData);
 
-        if (maxMagnitude > 0)
-        {
-            var data = spectrumData.GetData();
-            for (int i = 0; i < data.Length; i++)
-            {
-                data[i] /= maxMagnitude;
-            }
-        }
-    }
+		var data = spectrumData.GetData();
+		if (data.Length == 0)
+		{
+			return;
+		}
+
+		var maxMagnitude = data.Max();
+		if (maxMagnitude > 0)
+		{
+			for (int i = 0; i < data.Length; i++)
+			{
+				data[i] /= maxMagnitude;
+			}
+		}
+	}
 }
