1b8b9579d04819d276c1be706badaeb1ebd289f4
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-10 01:57:39 +0000

    chore: maintenance updates

--- src/Exceptions/AudioDeviceException.cs
+++ src/Exceptions/AudioDeviceException.cs
@@ -18,16 +18,36 @@ public class AudioDeviceException : Exception
     /// </summary>
     public int? DeviceIndex { get; set; }
 
+    /// <summary>
+    /// Initializes a new instance of the AudioDeviceException class with a specified error message.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
     public AudioDeviceException(string message) : base(message) { }
 
+    /// <summary>
+    /// Initializes a new instance of the AudioDeviceException class with a specified error message and the device index that caused the error.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="deviceIndex">The index of the audio device that caused the error.</param>
     public AudioDeviceException(string message, int deviceIndex) : base(message)
     {
         DeviceIndex = deviceIndex;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the AudioDeviceException class with a specified error message and a reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="innerException">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
     public AudioDeviceException(string message, Exception innerException)
         : base(message, innerException) { }
 
+    /// <summary>
+    /// Initializes a new instance of the AudioDeviceException class with a specified error message, the device index that caused the error, and a reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="deviceIndex">The index of the audio device that caused the error.</param>
+    /// <param name="innerException">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
     public AudioDeviceException(string message, int deviceIndex, Exception innerException)
         : base(message, innerException)
     {
