f40f50619bca358afce97a30280728be5e413d04
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-10 23:49:58 +0000

    refactor: improve ConfigurationManager extension methods

--- src/Configuration/ConfigurationManagerExtensions.cs
+++ src/Configuration/ConfigurationManagerExtensions.cs
@@ -1,92 +1,109 @@
-#nullable enable
-// =============================================================================
-// Author: Vladyslav Zaiets | https://sarmkadan.com
-// CTO & Software Architect
-// =============================================================================
+// <copyright file="ConfigurationManagerExtensions.cs" company="NAudioVisualizer">
+// Copyright (c) NAudioVisualizer. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
 
-using System;
-
-namespace NAudioVisualizer.Configuration;
-
-/// <summary>
-/// Extension methods for <see cref="ConfigurationManager"/> to provide convenient and strongly-typed configuration access.
-/// </summary>
-public static class ConfigurationManagerExtensions
+namespace NAudioVisualizer.Configuration
 {
-    /// <summary>
-    /// Gets a required configuration value. Throws <see cref="KeyNotFoundException"/> if the key is missing.
-    /// </summary>
-    /// <typeparam name="T">The type of the configuration value.</typeparam>
-    /// <param name="configuration">The configuration manager instance.</param>
-    /// <param name="key">The configuration key.</param>
-    /// <returns>The configuration value.</returns>
-    /// <exception cref="KeyNotFoundException">Thrown when the configuration key is not found.</exception>
-    public static T GetRequired<T>(this ConfigurationManager configuration, string key)
-    {
-        if (string.IsNullOrWhiteSpace(key))
-            throw new ArgumentException("Configuration key cannot be null, empty, or whitespace.", nameof(key));
+    using System;
 
-        if (!configuration.Contains(key))
+    /// <summary>
+    /// Provides extension methods for <see cref="ConfigurationManager"/>.
+    /// </summary>
+    public static class ConfigurationManagerExtensions
+    {
+        /// <summary>
+        /// Gets the required configuration value for the specified key.
+        /// </summary>
+        /// <typeparam name="T">The type of the configuration value.</typeparam>
+        /// <param name="configurationManager">The configuration manager.</param>
+        /// <param name="key">The configuration key.</param>
+        /// <returns>The configuration value.</returns>
+        /// <exception cref="KeyNotFoundException">Thrown when the key is not found.</exception>
+        public static T GetRequired<T>(this ConfigurationManager configurationManager, string key)
+        {
+            if (configurationManager == null)
+            {
+                throw new ArgumentNullException(nameof(configurationManager));
+            }
+
+            if (string.IsNullOrEmpty(key))
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (configurationManager.TryGetValue(key, out T? value))
+            {
+                return value!;
+            }
+
             throw new KeyNotFoundException($"Configuration key '{key}' not found.");
-
-        // GetValue returns null/default if conversion fails, but we've already verified the key exists.
-        // We rely on GetValue's conversion logic; if conversion fails, it returns default(T).
-        // For reference types, default(T) is null; for value types, it's 0/false/etc.
-        // This matches the behavior of GetValue when the key exists but value is invalid.
-        var value = configuration.GetValue<T>(key);
-        return value;
-    }
-
-    /// <summary>
-    /// Attempts to get a configuration value, returning whether the key exists and the value could be retrieved.
-    /// </summary>
-    /// <typeparam name="T">The type of the configuration value.</typeparam>
-    /// <param name="configuration">The configuration manager instance.</param>
-    /// <param name="key">The configuration key.</param>
-    /// <param name="value">When this method returns true, contains the configuration value; otherwise, the default value for <typeparamref name="T"/>.</param>
-    /// <returns>true if the key was found; otherwise, false.</returns>
-    public static bool TryGetValue<T>(this ConfigurationManager configuration, string key, out T value)
-    {
-        if (string.IsNullOrWhiteSpace(key))
-        {
-            value = default;
-            return false;
         }
 
-        if (configuration.Contains(key))
+        /// <summary>
+        /// Tries to get the configuration value for the specified key.
+        /// </summary>
+        /// <typeparam name="T">The type of the configuration value.</typeparam>
+        /// <param name="configurationManager">The configuration manager.</param>
+        /// <param name="key">The configuration key.</param>
+        /// <param name="value">When this method returns, contains the configuration value if the key is found; otherwise, the default value for the type of the value parameter.</param>
+        /// <returns>true if the key is found; otherwise, false.</returns>
+        public static bool TryGetValue<T>(this ConfigurationManager configurationManager, string key, out T value)
         {
-            value = configuration.GetValue<T>(key);
-            return true;
+            if (configurationManager == null)
+            {
+                throw new ArgumentNullException(nameof(configurationManager));
+            }
+
+            if (string.IsNullOrEmpty(key))
+            {
+                value = default!;
+                return false;
+            }
+
+            try
+            {
+                value = configurationManager.GetValue<T>(key);
+                return true;
+            }
+            catch
+            {
+                value = default!;
+                return false;
+            }
         }
 
-        value = default;
-        return false;
+        /// <summary>
+        /// Gets the configuration value as an integer.
+        /// </summary>
+        /// <param name="configurationManager">The configuration manager.</param>
+        /// <param name="key">The configuration key.</param>
+        /// <returns>The configuration value as an integer.</returns>
+        public static int GetInt(this ConfigurationManager configurationManager, string key)
+        {
+            return configurationManager.GetValue<int>(key);
+        }
+
+        /// <summary>
+        /// Gets the configuration value as a float.
+        /// </summary>
+        /// <param name="configurationManager">The configuration manager.</param>
+        /// <param name="key">The configuration key.</param>
+        /// <returns>The configuration value as a float.</returns>
+        public static float GetFloat(this ConfigurationManager configurationManager, string key)
+        {
+            return configurationManager.GetValue<float>(key);
+        }
+
+        /// <summary>
+        /// Gets the configuration value as a boolean.
+        /// </summary>
+        /// <param name="configurationManager">The configuration manager.</param>
+        /// <param name="key">The configuration key.</param>
+        /// <returns>The configuration value as a boolean.</returns>
+        public static bool GetBool(this ConfigurationManager configurationManager, string key)
+        {
+            return configurationManager.GetValue<bool>(key);
+        }
     }
-
-    /// <summary>
-    /// Gets an integer configuration value. Returns 0 if the key is missing or the value cannot be converted.
-    /// </summary>
-    /// <param name="configuration">The configuration manager instance.</param>
-    /// <param name="key">The configuration key.</param>
-    /// <returns>The integer configuration value.</returns>
-    public static int GetInt(this ConfigurationManager configuration, string key) =>
-        configuration.GetValue<int>(key, 0);
-
-    /// <summary>
-    /// Gets a floating-point configuration value. Returns 0.0 if the key is missing or the value cannot be converted.
-    /// </summary>
-    /// <param name="configuration">The configuration manager instance.</param>
-    /// <param name="key">The configuration key.</param>
-    /// <returns>The floating-point configuration value.</returns>
-    public static float GetFloat(this ConfigurationManager configuration, string key) =>
-        configuration.GetValue<float>(key, 0f);
-
-    /// <summary>
-    /// Gets a boolean configuration value. Returns false if the key is missing or the value cannot be converted.
-    /// </summary>
-    /// <param name="configuration">The configuration manager instance.</param>
-    /// <param name="key">The configuration key.</param>
-    /// <returns>The boolean configuration value.</returns>
-    public static bool GetBool(this ConfigurationManager configuration, string key) =>
-        configuration.GetValue<bool>(key, false);
 }
