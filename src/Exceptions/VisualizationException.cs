490cdcb1fd469c2e4a8ac5d55a412bf63a5a9e10
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-10 00:18:06 +0000

    docs: add XML documentation to VisualizationException constructors

--- src/Exceptions/VisualizationException.cs
+++ src/Exceptions/VisualizationException.cs
@@ -18,17 +18,37 @@ public class VisualizationException : Exception
     /// </summary>
     public string? VisualizationType { get; set; }
 
+    /// <summary>
+    /// Initializes a new instance of the VisualizationException class with a specified error message.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
     public VisualizationException(string message) : base(message) { }
 
+    /// <summary>
+    /// Initializes a new instance of the VisualizationException class with a specified error message and the visualization type that failed.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="visualizationType">The type of visualization that failed.</param>
     public VisualizationException(string message, string visualizationType)
         : base(message)
     {
         VisualizationType = visualizationType;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the VisualizationException class with a specified error message and a reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="innerException">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
     public VisualizationException(string message, Exception innerException)
         : base(message, innerException) { }
 
+    /// <summary>
+    /// Initializes a new instance of the VisualizationException class with a specified error message, the visualization type that failed, and a reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="visualizationType">The type of visualization that failed.</param>
+    /// <param name="innerException">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
     public VisualizationException(string message, string visualizationType, Exception innerException)
         : base(message, innerException)
     {
