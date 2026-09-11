ee6c7a43ba728cd61cf3d4014c7aec7fb78bb888
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-11 00:11:05 +0000

    feat: add CacheManagerExtensions with GetOrAdd, SetMany, and IsFull

--- src/Caching/CacheManagerExtensions.cs
+++ src/Caching/CacheManagerExtensions.cs
@@ -1,40 +1,36 @@
-#nullable enable
-// =============================================================================
-// Author: Vladyslav Zaiets | https://sarmkadan.com
-// CTO & Software Architect
-// =============================================================================
-
-using System;
-using System.Collections.Generic;
+// <copyright file="CacheManagerExtensions.cs" company="NAudioVisualizer">
+//     Copyright (c) NAudioVisualizer. All rights reserved.
+// </copyright>
 
 namespace NAudioVisualizer.Caching
 {
+    using System;
+    using System.Collections.Generic;
+
     /// <summary>
-    /// Extension methods for <see cref="CacheManager{TKey,TValue}"/>.
+    /// Extension methods for <see cref="CacheManager{TKey, TValue}"/>.
     /// </summary>
     public static class CacheManagerExtensions
     {
         /// <summary>
-        /// Gets the value associated with the specified key, or adds a new value using the factory function if the key is not present.
+        /// Gets the value associated with the specified key from the cache, or adds it using the factory if not present.
         /// </summary>
         /// <param name="cache">The cache manager instance.</param>
-        /// <param name="key">The key of the value to get or add.</param>
-        /// <param name="factory">A function that creates a new value for the key.</param>
-        /// <param name="expiration">Optional expiration time for the cached value. If null, uses the cache's default expiration.</param>
-        /// <returns>The value associated with the key.</returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="cache"/>, <paramref name="key"/>, or <paramref name="factory"/> is null.</exception>
+        /// <param name="key">The key of the value to get.</param>
+        /// <param name="factory">The function used to generate a value for the key.</param>
+        /// <param name="expiration">Optional expiration time for the newly added item.</param>
+        /// <returns>The value for the key, either retrieved or newly generated.</returns>
         public static TValue GetOrAdd<TKey, TValue>(this CacheManager<TKey, TValue> cache, TKey key, Func<TKey, TValue> factory, TimeSpan? expiration = null)
             where TKey : notnull
         {
-            if (cache is null)
-                throw new ArgumentNullException(nameof(cache));
-            if (key is null)
-                throw new ArgumentNullException(nameof(key));
-            if (factory is null)
-                throw new ArgumentNullException(nameof(factory));
+            if (cache == null) throw new ArgumentNullException(nameof(cache));
+            if (key == null) throw new ArgumentNullException(nameof(key));
+            if (factory == null) throw new ArgumentNullException(nameof(factory));
 
-            if (cache.TryGetValue(key, out var value))
-                return value;
+            if (cache.TryGetValue(key, out TValue? value))
+            {
+                return value!;
+            }
 
             var newValue = factory(key);
             cache.Set(key, newValue, expiration);
@@ -45,34 +41,29 @@ namespace NAudioVisualizer.Caching
         /// Sets multiple key-value pairs in the cache.
         /// </summary>
         /// <param name="cache">The cache manager instance.</param>
-        /// <param name="items">The key-value pairs to set in the cache.</param>
-        /// <param name="expiration">Optional expiration time for the cached values. If null, uses the cache's default expiration.</param>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="cache"/> or <paramref name="items"/> is null.</exception>
-        public static void SetMany<TKey, TValue>(this CacheManager<TKey, TValue> cache, IEnumerable<KeyValuePair<TKey, TValue>> items, TimeSpan? expiration = null)
+        /// <param name="items">The collection of key-value pairs to set.</param>
+        public static void SetMany<TKey, TValue>(this CacheManager<TKey, TValue> cache, IEnumerable<KeyValuePair<TKey, TValue>> items)
             where TKey : notnull
         {
-            if (cache is null)
-                throw new ArgumentNullException(nameof(cache));
-            if (items is null)
-                throw new ArgumentNullException(nameof(items));
+            if (cache == null) throw new ArgumentNullException(nameof(cache));
+            if (items == null) throw new ArgumentNullException(nameof(items));
 
             foreach (var item in items)
             {
-                cache.Set(item.Key, item.Value, expiration);
+                cache.Set(item.Key, item.Value);
             }
         }
 
         /// <summary>
-        /// Determines whether the cache is full (i.e., current size has reached or exceeded maximum size).
+        /// Determines whether the cache is full.
         /// </summary>
         /// <param name="cache">The cache manager instance.</param>
-        /// <returns>true if the cache is full; otherwise, false.</returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="cache"/> is null.</exception>
+        /// <returns><c>true</c> if the cache size is greater than or equal to the maximum size; otherwise, <c>false</c>.</returns>
         public static bool IsFull<TKey, TValue>(this CacheManager<TKey, TValue> cache)
             where TKey : notnull
         {
-            if (cache is null)
-                throw new ArgumentNullException(nameof(cache));
+            if (cache == null) throw new ArgumentNullException(nameof(cache));
+
             return cache.GetSize() >= cache.GetMaxSize();
         }
     }
