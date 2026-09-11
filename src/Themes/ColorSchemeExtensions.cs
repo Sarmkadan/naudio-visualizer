7930d005937d957f5732c556a40016b31b747e83
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-11 01:05:33 +0000

    chore: maintenance updates

--- src/Themes/ColorSchemeExtensions.cs
+++ src/Themes/ColorSchemeExtensions.cs
@@ -1,3 +1,9 @@
+#nullable enable
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
 using System;
 using System.Collections.Generic;
 using System.Drawing;
